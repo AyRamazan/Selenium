fe4c35885fc716c8b05849637738cd008f845f6f
AyRamazan <ramazanay.2363@gmail.com>
2023-03-10 14:10:25 +0300

    RadioButtons

--- src/test/java/Day05_Assertions_dropdownMenu/C02_Checkbox.java
+++ src/test/java/Day05_Assertions_dropdownMenu/C02_Checkbox.java
@@ -19,6 +19,57 @@ public class C02_Checkbox {
     //  b. Checkbox1 ve checkbox2 elementlerini locate edin.
     //  c. Checkbox1 seçili değilse onay kutusunu tıklayın
     //  d. Checkbox2 seçili değilse onay kutusunu tıklayın
+
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @After
+    public void teardown() {
+      driver.close();
+
+    }
+
+    @Test
+    public void test01() throws InterruptedException {
+        driver.get(" https://the-internet.herokuapp.com/checkboxes");
+        //  b. Checkbox1 ve checkbox2 elementlerini locate edin.
+        WebElement cb1 = driver.findElement(By.xpath(" (//input [@type='checkbox'])[1]"));
+        WebElement cb2 = driver.findElement(By.xpath(" (//input [@type='checkbox'])[2]"));
+        //  c. Checkbox1 seçili değilse onay kutusunu tıklayın
+
+        Thread.sleep(3000);
+        if (!cb1.isSelected()) {
+            cb1.click();
+
+        }
+
+        //  d. Checkbox2 seçili değilse onay kutusunu tıklayın
+        if (!cb2.isSelected()) {
+
+        }
+        cb2.click();
+
+        //e. checkbox1 ve checkbox2'nin seçili oldugunu test edin
+
+        Assert.assertTrue(cb1.isSelected());
+
+        Assert.assertTrue(cb2.isSelected());
+
+    }
+}
+
+
+    /*
+
     WebDriver driver;
     @Before
     public void setup(){
@@ -52,4 +103,5 @@ public class C02_Checkbox {
         Assert.assertTrue(cb1.isSelected());
         Assert.assertTrue(cb2.isSelected());
     }
-}
+
+     */
--- src/test/java/Day05_Assertions_dropdownMenu/C03_RadioButtons.java
+++ src/test/java/Day05_Assertions_dropdownMenu/C03_RadioButtons.java
@@ -20,6 +20,50 @@ public class C03_RadioButtons {
     //  c. Create an account buton’una basin
     //  d. Radio button elementlerini locate edin ve size uygun olani secin
     //  e. Sectiginiz radio butoon'un secili, otekilerin ise secili olmadigini test edin
+
+
+    WebDriver driver;
+    @Before
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @After
+    public void teardown(){
+        driver.close();
+
+    }
+
+    @Test
+    public void test01(){
+        driver.get("https://facebook.com");
+
+        //  b. Cookies’i kabul edin
+        driver.findElement(By.xpath("//button[text()='Allow essential and optional cookies']"))
+                .click();
+        //  c. Create an account buton’una basin
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        //  d. Radio button elementlerini locate edin ve size uygun olani secin
+        WebElement femaleElementi= driver.findElement(By.xpath("(//input[@type='radio'])[1]"));
+        WebElement maleElementi= driver.findElement(By.xpath("(//input[@type='radio'])[2]"));
+        WebElement customElementi= driver.findElement(By.xpath("(//input[@type='radio'])[3]"));
+
+        maleElementi.click();
+        //  e. Sectiginiz radio button'un secili, otekilerin ise secili olmadigini test edin
+        Assert.assertTrue(maleElementi.isSelected());
+        Assert.assertFalse(femaleElementi.isSelected());
+        Assert.assertFalse(customElementi.isSelected());
+    }
+
+}
+
+    /*
+
+
     WebDriver driver;
     @Before
     public void setup(){
@@ -50,5 +94,7 @@ public class C03_RadioButtons {
         Assert.assertFalse(femaleElementi.isSelected());
         Assert.assertFalse(customElementi.isSelected());
     }
-}
+
+     */
+
 
