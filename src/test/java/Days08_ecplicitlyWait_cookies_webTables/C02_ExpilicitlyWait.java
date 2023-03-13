2e7b5dd9edc07ceef2ea598ee170f19fec8c71ce
AyRamazan <ramazanay.2363@gmail.com>
2023-03-13 08:50:04 +0300

    Waits

---
+++ src/test/java/Days08_ecplicitlyWait_cookies_webTables/C02_ExpilicitlyWait.java
@@ -0,0 +1,27 @@
+package Days08_ecplicitlyWait_cookies_webTables;
+
+public class C02_ExpilicitlyWait {
+    /*
+
+    @Test
+    public void test01() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        // amazon anasayfaya gidin
+        driver.get("https://www.amazon.com");
+        // arama kutusu clickable oluncaya kadar bekleyin
+        // explicitly wait icin
+        //  A- wait objesi olustur
+        WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(40));
+        // B - mumkunse webelementi locate et
+        WebElement aramaKutusu= driver.findElement(By.id("twotabsearchtextbox"));
+        // C- wait objesini kullanarak istenen gorev icin bekle
+        wait.until(ExpectedConditions.elementToBeClickable(aramaKutusu));
+        // Nutella icin arama yapin
+        aramaKutusu.sendKeys("Nutella" + Keys.ENTER);
+        Thread.sleep(5000);
+        driver.close();
+    }
+}
+
