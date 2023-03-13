f14ecde1975b1c52033e144a95b9c1f2db6406e1
AyRamazan <ramazanay.2363@gmail.com>
2023-03-13 07:28:13 +0300

    FileExist

--- src/test/java/Days07_actionsClass_fileTestleri/C06_FileExistTesti.java
+++ src/test/java/Days07_actionsClass_fileTestleri/C06_FileExistTesti.java
@@ -1,6 +1,56 @@
 package Days07_actionsClass_fileTestleri;
 
-public class C06_FileExistTesti {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C06_FileExistTesti extends TestBase {
+
+
+    @Test
+    public void test01(){
+
+        //2. https://the-internet.herokuapp.com/download adresine gidelim.
+        driver.get("https://the-internet.herokuapp.com/download");
+
+        //3. logo.png dosyasını indirelim
+        driver.findElement(By.linkText("logo.png")).click();
+
+        //4. Dosyanın başarıyla indirilip indirilmediğini test edelim
+
+        bekle(5);
+
+        // dosya dowloads'a ineceği icin dinamik dosya yolu olusturalım
+        String dinamikDosyaYolu= System.getProperty("user.home")+"/Dowloads/logo.png";
+
+        Assert.assertTrue(Files.exists(Paths.get(dinamikDosyaYolu)));
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /*
 
     @Test
